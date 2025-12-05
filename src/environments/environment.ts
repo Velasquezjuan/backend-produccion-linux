a180525c83a909da9af00ec963be211538cc57f1
velasquezJuan <velasquez.ocares@hotmail.com>
2025-12-05 13:53:18 -0300

    Cambio enlace backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
  production: false,
-  apiUrl: 'https://backend-programacion-rbgx.onrender.com'
+  apiUrl: 'https://backend-produccion-linux.onrender.com/'
 }
 /*
  * For easier debugging in development mode, you can import the following file
